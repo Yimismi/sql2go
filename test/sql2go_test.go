83e3fdabe4e4011c4ae33da3f52a366ce8b8d57e
yimismi <1143937551@qq.com>
2019-06-26 01:35:30 +0800

    add test

--- test/sql2go_test.go
+++ test/sql2go_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestFromFile1(t *testing.T) {
-	ares := sql2go.NewConvertArgs().SetGenJson(true).SetPackageName("test")
-	code, err := sql2go.FromFile("./1.sql", ares)
+	args := sql2go.NewConvertArgs().SetGenJson(true).SetPackageName("test")
+	code, err := sql2go.FromFile("./1.sql", args)
 	if err != nil {
 		t.Error(err)
 		return
@@ -22,17 +22,45 @@ func TestFromFile1(t *testing.T) {
 	f.Write(code)
 }
 func TestFromFile2(t *testing.T) {
-	ares := sql2go.NewConvertArgs().SetGenJson(true).
+	args := sql2go.NewConvertArgs().
 		SetPackageName("test").SetTmpl(sql2go.GOTMPL).
 		SetColPrefix("f_").
 		SetTablePrefix("t_")
 
-	code, err := sql2go.FromFile("./2.sql", ares)
+	code, err := sql2go.FromFile("./2.sql", args)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	f, err := os.Create("db_struct2.go")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer f.Close()
+	f.Write(code)
+}
+
+func TestFromSql1(t *testing.T) {
+	sql := `
+CREATE TABLE IF NOT EXISTS t_person (
+  f_age INT(11) NULL,
+  f_id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  f_name VARCHAR(30) NOT NULL,
+  f_sex VARCHAR(2) NULL
+  ) ENGINE=InnoDB;
+`
+	args := sql2go.NewConvertArgs().SetGenJson(true).
+		SetPackageName("test").
+		SetColPrefix("f_").
+		SetTablePrefix("t_")
+
+	code, err := sql2go.FromSql(sql, args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	f, err := os.Create("db_struct3.go")
 	if err != nil {
 		t.Error(err)
 		return
